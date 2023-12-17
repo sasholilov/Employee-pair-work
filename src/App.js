ec7fec87b8be1b7f582cddf03ab14e340777b859
sasholilov <alex_xld@abv.bg>
2023-12-17 15:47:21 +0200

    Upload and read the csv file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "./components/Buttons/Button";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  function handleFileChange(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function () {
+      const data = reader.result;
+      console.log(data);
+    };
+  }
   return (
     <div className="App">
       <h1>Employee pair work</h1>
-      <Button type="file" text="Please upload CSV file"></Button>
+      <Button
+        type="file"
+        text="Please upload CSV file"
+        onHandleChange={handleFileChange}
+      ></Button>
     </div>
   );
 }
