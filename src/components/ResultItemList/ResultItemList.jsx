c5a3167c51e294f9d8225188c5a739da8d7a0111
sasholilov <alex_xld@abv.bg>
2023-12-18 22:14:36 +0200

    create new function to group by pair of employees in new array with objects and to push in nested array common projects with properties. Create funcition to calculate the common working days

--- src/components/ResultItemList/ResultItemList.jsx
+++ src/components/ResultItemList/ResultItemList.jsx
@@ -1,12 +1,12 @@
 import ResultItem from "../ResultItem/ResultItem";
 import "./ResultItemList.css";
-import { commonProjectsArray, procesDate } from "../../utils/procesData";
+import { pairEmployeWithProjects, procesDate } from "../../utils/procesData";
 
 import React from "react";
 
 function ResultItemList({ data }) {
   procesDate(data);
-  const finalData = commonProjectsArray(data);
+  const finalData = pairEmployeWithProjects(data);
   {
     return finalData.map((item, index) => (
       <ResultItem key={index} data={item} />
--- src/utils/procesData.js
+++ src/utils/procesData.js
@@ -9,15 +9,15 @@ const procesDate = function (array) {
   });
 };
 
-const commonProjectsArray = function (arrayData) {
+const commonProjects = function (arrayData) {
   const tempArray = [];
   arrayData.forEach((item) => {
-    let newObj = {};
+    let tempObj = {};
     const projectIndex = tempArray.findIndex(
       (f) => f.projectId === item.projectId
     );
     if (projectIndex === -1) {
-      newObj = {
+      tempObj = {
         projectId: item.projectId,
         employeeOne: item.employeeId,
         employeeOneDateFrom: item.dateFrom,
@@ -26,15 +26,86 @@ const commonProjectsArray = function (arrayData) {
         employeeTwoDateFrom: "",
         employeeTwoDateTo: "",
       };
-      tempArray.push(newObj);
+      tempArray.push(tempObj);
     } else {
       tempArray[projectIndex].employeeTwo = item.employeeId;
       tempArray[projectIndex].employeeTwoDateFrom = item.dateFrom;
       tempArray[projectIndex].employeeTwoDateTo = item.dateTo;
     }
   });
-  console.log("checkpoint", tempArray);
   return tempArray;
 };
 
-export { procesDate, commonProjectsArray };
+const pairEmployeWithProjects = function (arrayData) {
+  const tempArray = [];
+  const commonProjectsArr = commonProjects(arrayData);
+  commonProjectsArr.forEach((item) => {
+    let tempObj = {};
+    const pairEmpIndex = tempArray.findIndex(
+      (f) =>
+        (f.employeeOne === item.employeeOne &&
+          f.employeeTwo === item.employeeTwo) ||
+        (f.employeeOne === item.employeeTwo &&
+          f.employeeTwo === item.employeeOne)
+    );
+    if (pairEmpIndex === -1) {
+      tempObj = {
+        employeeOne: item.employeeOne,
+        employeeTwo: item.employeeTwo,
+        commonProjects: [
+          {
+            id: item.projectId,
+            employeeOneDateFrom: item.employeeOneDateFrom,
+            employeeOneDateTo: item.employeeOneDateTo,
+            employeeTwoDateFrom: item.employeeTwoDateFrom,
+            employeeTwoDateTo: item.employeeTwoDateTo,
+            commonWorkingDays: calcCommonDays(
+              item.employeeOneDateFrom,
+              item.employeeOneDateTo,
+              item.employeeTwoDateFrom,
+              item.employeeTwoDateTo
+            ),
+          },
+        ],
+      };
+      tempArray.push(tempObj);
+    } else {
+      tempArray[pairEmpIndex].commonProjects.push({
+        id: item.projectId,
+        employeeOneDateFrom: item.employeeOneDateFrom,
+        employeeOneDateTo: item.employeeOneDateTo,
+        employeeTwoDateFrom: item.employeeTwoDateFrom,
+        employeeTwoDateTo: item.employeeTwoDateTo,
+        commonWorkingDays: calcCommonDays(
+          item.employeeOneDateFrom,
+          item.employeeOneDateTo,
+          item.employeeTwoDateFrom,
+          item.employeeTwoDateTo
+        ),
+      });
+    }
+    console.log(
+      calcCommonDays(
+        item.employeeOneDateFrom,
+        item.employeeOneDateTo,
+        item.employeeTwoDateFrom,
+        item.employeeTwoDateTo
+      )
+    );
+  });
+  const finalArray = tempArray.filter((e) => e.commonProjects.length > 1);
+  console.log(finalArray);
+
+  return finalArray;
+};
+
+const calcCommonDays = function (startOne, endOne, startTwo, endTwo) {
+  if (endOne < startTwo || endTwo < startOne) {
+    return 0;
+  }
+
+  const commonDays = Math.min(endOne, endTwo) - Math.max(startOne, startTwo);
+  return Math.floor(commonDays / (24 * 60 * 60 * 1000)) + 1;
+};
+
+export { procesDate, commonProjects, pairEmployeWithProjects };
