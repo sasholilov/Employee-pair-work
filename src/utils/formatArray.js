13c3e72f8877b60a1e447d4d6c393109c23d946a
sasholilov <alex_xld@abv.bg>
2023-12-19 22:38:29 +0200

    added to read date format: ddmmYYYY

--- src/utils/formatArray.js
+++ src/utils/formatArray.js
@@ -1,13 +1,22 @@
 const convertDateData = function (value) {
+  let separator;
   if (value === "NULL") {
     return "NULL";
   }
 
-  let separator;
   if (value.includes("-")) {
     separator = "-";
   } else if (value.includes(".")) {
     separator = ".";
+  }
+
+  if (!separator) {
+    const day = value.slice(0, 2);
+    const month = value.slice(2, 4);
+    const year = value.slice(4, 8);
+    const formatedDateString = `${year}-${month}-${day}`;
+    console.log(formatedDateString);
+    return formatedDateString;
   }
 
   const tempArr = value.split(separator);
@@ -16,8 +25,10 @@ const convertDateData = function (value) {
     .join("-")
     .trim();
 
-  if (convertedString.split("-")[0].length > 2) {
-    const year = convertedString.split("-")[0];
+  const stringFirstItemLength = convertedString.split("-")[0].length;
+
+  if (stringFirstItemLength > 2) {
+    const year = convertedString.split("-")[0].slice(0, 4);
     const month = convertedString.split("-")[1];
     const day = convertedString.split("-")[2];
     const formatedDateString = `${year}-${month}-${day}`;
