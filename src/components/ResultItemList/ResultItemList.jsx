a5e58313005b058fba572e216d65f0d974ad6d03
sasholilov <alex_xld@abv.bg>
2023-12-22 23:24:13 +0200

    fix filter function to work propper and show text when there are not results from the filter

--- src/components/ResultItemList/ResultItemList.jsx
+++ src/components/ResultItemList/ResultItemList.jsx
@@ -14,16 +14,23 @@ function ResultItemList({ data }) {
   const [searchArr, setSearchArr] = useState([]);
   const [searchValueEmp, setSearchValueEmp] = useState("");
   const [searchValuePrj, setSearchValuePrj] = useState("");
+
   const finalData = pairEmployeWithProjects(data);
+
+  useEffect(() => {
+    setSearchArr(finalData);
+  }, [data]);
 
   function searchHandleEmp(e) {
     const value = e.target.value;
     setSearchValueEmp(value);
   }
+
   function searchHandlePrj(e) {
     const value = e.target.value;
     setSearchValuePrj(value);
   }
+
   useEffect(() => {
     const tempArr = searchByEmployeeId(finalData, searchValueEmp);
     setSearchArr(tempArr);
@@ -34,8 +41,6 @@ function ResultItemList({ data }) {
     setSearchArr(tempArr);
   }, [searchValuePrj]);
 
-  const resultArr = searchArr.length ? searchArr : finalData;
-
   return (
     <div className="container">
       <div className="result-items">
@@ -43,12 +48,12 @@ function ResultItemList({ data }) {
           <div className="result-items-header">
             <SearchBar searchHandle={searchHandleEmp} />
             <SearchBar searchHandle={searchHandlePrj} />
-            <h2>Result</h2>
           </div>
         )}
-        {resultArr.map((item, index) => (
+        {searchArr.map((item, index) => (
           <ResultItem key={index} data={item} />
         ))}
+        {!searchArr.length && <h2>No results to show</h2>}
       </div>
     </div>
   );
